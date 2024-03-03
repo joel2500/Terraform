3330410c11e7ccf7f039be8e178c6f3efdd52b12
sharon <sharonraja207@gmail.com>
2024-03-03 18:25:59 +0530

    adding folder

---
+++ QA/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_access_key" {
+  description = "AKIA3FLD4EJP2QLQTQW4"
+}
+
+variable "aws_secret_key" {
+  description = "MCizMUMkPTVWtl6b76r6japCCP+WNHbDS1f8yUWU"
+}
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_access_key" {
-  description = "AKIA3FLD4EJP2QLQTQW4"
-}
-
-variable "aws_secret_key" {
-  description = "MCizMUMkPTVWtl6b76r6japCCP+WNHbDS1f8yUWU"
-}
